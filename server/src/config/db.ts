83f3e6d01ea1ce46b0bfd553331bb50badbe9c13
Jay <jay@Jays-MBP.STELFIBER>
2026-04-08 10:18:39 +0530

    logs added

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -1,9 +1,14 @@
 import mongoose from 'mongoose';
 
 export async function connectDB(): Promise<void> {
-  const uri = process.env.MONGODB_URI;
+  try {
+     const uri = process.env.MONGODB_URI;
   if (!uri) throw new Error('MONGODB_URI is not set');
 
   await mongoose.connect(uri);
   console.log('MongoDB connected');
+  } catch (error) {
+    console.log(error)
+  }
+ 
 }
