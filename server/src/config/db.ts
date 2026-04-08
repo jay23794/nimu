b3d89ac3435bd437f57f752c9c06557922cf7201
Jay <jay@Jays-MBP.STELFIBER>
2026-04-08 10:39:36 +0530

    dep log

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -2,6 +2,8 @@ import mongoose from 'mongoose';
 
 export async function connectDB(): Promise<void> {
   try {
+     console.log("URL---------->");
+       console.log( process.env.MONGODB_URI);
      const uri = process.env.MONGODB_URI;
   if (!uri) throw new Error('MONGODB_URI is not set');
 
